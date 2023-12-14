977a854f6dbb01feed9d2e22410db5c23ea02553
서인 <seoin2389@icloud.com>
2023-12-14 21:28:58 +0900

    FEAT update VO, Entity

--- EarBee/src/main/java/com/eb/earbee/business/repository/BusinessRepository.java
+++ EarBee/src/main/java/com/eb/earbee/business/repository/BusinessRepository.java
@@ -1,4 +1,12 @@
 package com.eb.earbee.business.repository;
 
-public class BusinessRepository {
+import com.eb.earbee.business.entity.Business;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.ArrayList;
+
+public interface BusinessRepository extends CrudRepository<Business, Long> {
+
+    @Override
+    ArrayList<Business> findAll();
 }
