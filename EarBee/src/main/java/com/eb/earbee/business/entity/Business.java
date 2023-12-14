30d4d42445d1493d5ef10273887260b978363c76
서인 <seoin2389@icloud.com>
2023-12-14 18:10:27 +0900

    FEAT : change DB - ENtity Bean 생성

--- EarBee/src/main/java/com/eb/earbee/business/entity/Business.java
+++ EarBee/src/main/java/com/eb/earbee/business/entity/Business.java
@@ -1,4 +1,13 @@
 package com.eb.earbee.business.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Business {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
