68ab0523f9966ac775235b602e1c9725ac793ce0
서인 <seoin2389@icloud.com>
2023-12-15 22:51:11 +0900

    FEAT : BusinessApiController - 모달창에서 입력받은 사업자 번호를 RESTAPI에 넘기기 추가

--- EarBee/src/main/java/com/eb/earbee/business/api/BusinessApiController.java
+++ EarBee/src/main/java/com/eb/earbee/business/api/BusinessApiController.java
@@ -1,49 +1,46 @@
 package com.eb.earbee.business.api;
 
 
-import com.eb.earbee.business.entity.Business;
+
 import com.eb.earbee.business.dto.BusinessApplyDto;
+
 import jakarta.xml.ws.Response;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/business")
 @PropertySource("classpath:business.properties")
 public class BusinessApiController {
 
     @Value("${business.url}")
-    private String url;
+    private String urlBusiness;
 
     @Value("${business.encoding}")
-    private String encoding;
+    private String encodingBusiness;
     @Value("${business.decoding}")
-    private String decoding;
+    private String decodingBusiness;
 
 
     // 정상적으로 변수를 가져오는지 체크
     @GetMapping()
     public List<String> checkValue(){
-        return Arrays.asList(url,encoding,decoding);
+        return Arrays.asList(urlBusiness,encodingBusiness,decodingBusiness);
     }
 
 
     // 모달창에서 넣은 사업자 번호를 조회
     @PostMapping("/search")
-    public Response<Business> businessSearch(@RequestBody BusinessApplyDto businessApplyDto){
-        String url = "";
-
-
-
-
-
-
+    public <T> Response<T> businessSearch(@RequestBody BusinessApplyDto businessApplyDto){
+        String url = this.urlBusiness;
+        System.out.println(businessApplyDto.toString());
 
         return null;
     }
