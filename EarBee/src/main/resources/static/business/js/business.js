61f704dcc56c249045dbf2bd72835f86380aebee
서인 <seoin2389@icloud.com>
2024-01-05 22:43:01 +0900

    FEAT Business Addr - 클릭 이벤트 추가

--- EarBee/src/main/resources/static/business/js/business.js
+++ EarBee/src/main/resources/static/business/js/business.js
@@ -26,7 +26,7 @@ if (Modal) {
         // type 별로 placeholder 바꾸기 및 현재 폼이 어떤 RestApi을 호출할 것인지 form에 data-sort-type을 지정함
         if (modalType !== 'business') {
             document.getElementById("inputNumber").value = "";
-            document.querySelector('#inputNumber').setAttribute("placeholder","매곡푸르지오");
+            document.querySelector('#inputNumber').setAttribute("placeholder", "매곡푸르지오");
             document.querySelector('#searchType').setAttribute('data-sort-type', 'address')
             document.querySelector('#inputNumber').setAttribute('type', 'text');
         } else {
@@ -41,14 +41,12 @@ if (Modal) {
 }
 
 
-
-
 function search() { // 조회가 사업자 조회인지 주소 검색인지 분류
     const dataSortType = document.querySelector('#searchType').getAttribute('data-sort-type');
 
     if (!(dataSortType !== 'businessNum')) {
         searchBusiness();
-    }else{
+    } else {
         searchAddr();
     }
 }
@@ -66,12 +64,10 @@ function isBusinessNumValid(businessNum) {
 }
 
 
-
-
-function searchBusiness(){
+function searchBusiness() {
     const businessNum = document.querySelector('#inputNumber').value;
 
-    if(!isBusinessNumValid(businessNum)){
+    if (!isBusinessNumValid(businessNum)) {
         return false;
     } // 사업자 번호가 정상적인지 체크
 
@@ -89,17 +85,17 @@ function searchBusiness(){
     }).then(response => {
         if (response.ok) {
             return response.json();
-        }else {
+        } else {
             alert("사업자 번호를 확인하세요");
             $('#inputNumber').val('').focus();
         }
     }).then(data => {
-        if(data!=null) {
+        if (data != null) {
             // conso0le.log('Received data:', data);
             $('#businessNum').val(data.b_no); // 메인 폼에 사업자 번호 삽입
             $("#inputNumber").val('');
             $('#Modal').modal('hide'); // 모달창 숨기기
-        }else{
+        } else {
             return false;
         }
 
@@ -111,12 +107,12 @@ function searchBusiness(){
 }
 
 // 주소 검색 필터링
-function checkAddr(obj){
-    if(obj.value.length >0){
+function checkAddr(obj) {
+    if (obj.value.length > 0) {
         //특수문자 제거
         const expText = /[%=><]/;
-        if(expText.test(obj.value) === true){
-            alert(`특수문자를 입력 할수 없습니다.`) ;
+        if (expText.test(obj.value) === true) {
+            alert(`특수문자를 입력 할수 없습니다.`);
             obj.value = obj.split(expText).join("");
             return false;
         }
@@ -126,23 +122,21 @@ function checkAddr(obj){
             "UNION", "FETCH", "DECLARE", "TRUNCATE"];
 
         let regex;
-        for(let i=0; i<sqlArray.length; i++){
-            regex = new RegExp( sqlArray[i] ,"gi") ;
+        for (let i = 0; i < sqlArray.length; i++) {
+            regex = new RegExp(sqlArray[i], "gi");
 
-            if (regex.test(obj.value) ) {
-                alert("\"" + sqlArray[i]+"\"와(과) 같은 특정문자로 검색할 수 없습니다.");
-                obj.value =obj.value.replace(regex, "");
+            if (regex.test(obj.value)) {
+                alert("\"" + sqlArray[i] + "\"와(과) 같은 특정문자로 검색할 수 없습니다.");
+                obj.value = obj.value.replace(regex, "");
                 return false;
             }
         }
-    }else{
+    } else {
         alert(`주소를 입력해주세요`);
         return false;
     }
-    return true ;
+    return true;
 }
-
-
 
 
 function searchAddr() {
@@ -155,7 +149,7 @@ function searchAddr() {
     }
 
     const url = "/api/business/addr";
-    const body = { keyword: addr.value };
+    const body = {keyword: addr.value};
 
     fetch(url, {
         method: "POST",  // HTTP 메서드는 대문자로 지정
@@ -193,18 +187,24 @@ function searchAddr() {
 function displaySearchResults(results) {
     const searchResultsDiv = document.getElementById('searchResults');
     searchResultsDiv.innerHTML = ''; // 이전에 표시된 결과를 초기화
+
+
     // 결과를 표시할 테이블 생성
     const table = document.createElement('table');
-    table.classList.add('table'); // Bootstrap의 테이블 스타일을 사용하려면 'table' 클래스를 추가
+
+    table.classList.add('table', 'table-scroll'); // Bootstrap의 테이블 스타일을 사용하려면 'table' 클래스를 추가
 
     // 테이블 헤더 생성
     const headerRow = document.createElement('tr');
     const nameLabel = document.createElement('th');
     nameLabel.textContent = '우편번호'; // 결과의 필드에 따라 수정
+    nameLabel.style.width = '20%'; //
+    nameLabel.style.textAlign = 'center'; // 텍스트 정렬 설정
     headerRow.appendChild(nameLabel);
 
     const additionalInfoLabel = document.createElement('th');
     additionalInfoLabel.textContent = '도로명 주소'; // 추가 정보의 필드에 따라 수정
+    additionalInfoLabel.style.textAlign = 'center';
     headerRow.appendChild(additionalInfoLabel);
 
     // 헤더를 테이블에 추가
@@ -213,15 +213,23 @@ function displaySearchResults(results) {
     // 결과를 순회하면서 각 행을 생성하여 추가
     results.forEach(result => {
         const row = document.createElement('tr');
+        row.onclick=function(event){
+                alert(`주소 클릭함: ${event.target.textContent}`);
+            };
 
         // 결과 필드에 따라 수정
         const nameCell = document.createElement('td');
         nameCell.textContent = result.zipNo;
+        nameCell.style.width = '20%'; // 스타일 직접 설정
+        nameCell.style.textAlign = 'center';
         row.appendChild(nameCell);
 
         // 추가 정보 필드에 따라 수정
         const additionalInfoCell = document.createElement('td');
-        additionalInfoCell.textContent = result.roadAddr; // 추가 정보가 없으면 빈 문자열 처리
+        const link = document.createElement('a');
+        link.textContent = result.roadAddr; // 추가 정보가 없으면 빈 문자열 처리
+        additionalInfoCell.appendChild(link);
+
         row.appendChild(additionalInfoCell);
 
         // 행을 테이블에 추가
@@ -229,6 +237,6 @@ function displaySearchResults(results) {
     });
 
     // 테이블을 결과 표시 영역에 추가
-
     searchResultsDiv.appendChild(table);
 }
+
