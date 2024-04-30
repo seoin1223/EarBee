f06eaba7aa8a9a927f9949b082972bd341b5f2f0
seoin <seoin2389@icloud.com>
2024-04-30 21:28:11 +0900

    add mumbership controller - 멤버쉽 컨트롤러 추가

--- EarBee/src/main/java/com/eb/earbee/mumbership/MembershipController.java
+++ EarBee/src/main/java/com/eb/earbee/mumbership/MembershipController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/earbee")
-public class MumbershipController {
+public class MembershipController {
 
     @GetMapping("/mypage")
     public String mypage(){
-        return "mumbershop/mumbership";
+        return "mumbership/mumbership";
     }
 }
