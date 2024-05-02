73d1f12a80a5caf0b748f6ecb163310d2d76c0b7
seoin <seoin2389@icloud.com>
2024-05-02 19:58:09 +0900

    add security 추가

---
+++ EarBee/src/main/java/com/eb/earbee/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.eb.earbee.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize ->
+                        authorize
+
+                                .anyRequest().permitAll() // 다른 모든 요청은 인증 없이 허용됩니다
+                );
+
+        return http
+                .build();
+    }
+}
