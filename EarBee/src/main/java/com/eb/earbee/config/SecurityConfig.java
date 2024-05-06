17c11a4f2cc7852521a886453a3e945956cb3f31
seoin <seoin2389@icloud.com>
2024-05-06 18:12:47 +0900

    add security config - add config

--- EarBee/src/main/java/com/eb/earbee/config/SecurityConfig.java
+++ EarBee/src/main/java/com/eb/earbee/config/SecurityConfig.java
@@ -1,12 +1,14 @@
 package com.eb.earbee.config;
 
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -17,8 +19,9 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize ->
                         authorize
-
-                                .anyRequest().permitAll() // 다른 모든 요청은 인증 없이 허용됩니다
+                                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/earbee/**")).hasAnyRole("ADMIN", "MANAGER","USER")
+                                .anyRequest().permitAll()
                 );
 
         return http
