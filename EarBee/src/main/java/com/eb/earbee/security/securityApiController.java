9045e8ad178b4fbcb7aef09f59e90cf8c1f75a1f
seoin <seoin2389@icloud.com>
2024-08-07 12:42:15 +0900

    refactoring Access myPage, header - earbee 이미지 추가 - ajax가 아닌 기본 config를 이용한 로그인

---
+++ EarBee/src/main/java/com/eb/earbee/security/securityApiController.java
@@ -0,0 +1,14 @@
+package com.eb.earbee.security;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/security")
+public class securityApiController {
+
+
+
+
+}
